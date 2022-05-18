ca75977380ad079f5784ceecbc414a779bb47c2b
m4rcer 4 <m4rcer@gmail.com>
2022-05-18 13:56:57 +0300

    Архитектура бекенд приложения

---
+++ SpaceXinfo.Backend/SpaceXinfo.Domain/Launch.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SpaceXinfo.Domain
+{
+    public class Launch
+    {
+        public Guid UserId { get; set; }
+        public Guid Id { get; set; }
+        public int FavouriteLaunchId { get; set; }
+    }
+}
